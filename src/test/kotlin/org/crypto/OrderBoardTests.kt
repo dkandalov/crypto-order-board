1b43aba1d5ac239f542ac250310df68bb5016457
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2020-09-30 09:32:52 +0100

    initial commit

---
+++ src/test/kotlin/org/crypto/OrderBoardTests.kt
@@ -0,0 +1,12 @@
+package org.crypto
+
+import com.natpryce.hamkrest.assertion.assertThat
+import com.natpryce.hamkrest.equalTo
+import org.junit.jupiter.api.Test
+
+
+class OrderBoardTests {
+    @Test fun `hello`() {
+        assertThat(42, equalTo(42))
+    }
+}
