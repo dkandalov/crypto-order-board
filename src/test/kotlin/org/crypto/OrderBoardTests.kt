f2e44659c8c6f2a67b6deaf76605a29cff7c657d
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2020-09-30 09:44:50 +0100

    summary for a single sell/buy order

--- src/test/kotlin/org/crypto/OrderBoardTests.kt
+++ src/test/kotlin/org/crypto/OrderBoardTests.kt
@@ -1,6 +1,7 @@
 package org.crypto
 
 import com.natpryce.hamkrest.assertion.assertThat
+import com.natpryce.hamkrest.equalTo
 import com.natpryce.hamkrest.isEmpty
 import org.crypto.OrderType.Buy
 import org.crypto.OrderType.Sell
@@ -9,17 +10,50 @@ import java.math.BigDecimal
 
 
 class OrderBoardTests {
+    private val someCoin = CoinType("someCoin")
+    private val someUser = UserId("someUserId")
+
     @Test fun `summary for empty order board`() {
         val emptyOrderBoard = OrderBoard()
-        val someCoin = CoinType("someCoin")
         assertThat(emptyOrderBoard.summary(Buy, someCoin), isEmpty)
         assertThat(emptyOrderBoard.summary(Sell, someCoin), isEmpty)
     }
+
+    @Test fun `summary for a single sell order`() {
+        val orderBoard = OrderBoard()
+        orderBoard.place(Order(Sell, someUser, someCoin, Quantity(1), Price(2)))
+
+        assertThat(
+            orderBoard.summary(Sell, someCoin),
+            equalTo(listOf(SummaryRow(Quantity(1), Price(2))))
+        )
+        assertThat(orderBoard.summary(Buy, someCoin), isEmpty)
+    }
+
+    @Test fun `summary for a single buy order`() {
+        val orderBoard = OrderBoard()
+        orderBoard.place(Order(Buy, someUser, someCoin, Quantity(3), Price(4)))
+
+        assertThat(
+            orderBoard.summary(Buy, someCoin),
+            equalTo(listOf(SummaryRow(Quantity(3), Price(4))))
+        )
+        assertThat(orderBoard.summary(Sell, someCoin), isEmpty)
+    }
+
 }
 
 class OrderBoard {
+    private val orders = ArrayList<Order>()
+
+    fun place(order: Order) {
+        orders.add(order)
+    }
+
     fun summary(orderType: OrderType, coinType: CoinType): List<SummaryRow> {
-        return emptyList()
+        return orders
+            .filter { it.type == orderType }
+            .map { SummaryRow(it.quantity, it.pricePerCoin) }
     }
 }
 
@@ -28,12 +62,30 @@ data class SummaryRow(
     val pricePerCoin: Price
 )
 
+data class Order(
+    val type: OrderType,
+    val userId: UserId,
+    val coinType: CoinType,
+    val quantity: Quantity,
+    val pricePerCoin: Price
+)
+
 enum class OrderType {
     Buy, Sell
 }
 
+data class UserId(val value: String)
+
 data class CoinType(val value: String)
 
-data class Quantity(val value: BigDecimal)
+data class Quantity(val value: BigDecimal) {
+    constructor(value: Int) : this(value.toBigDecimal())
 
-data class Price(val value: BigDecimal)
+    override fun toString() = value.toString()
+}
+
+data class Price(val value: BigDecimal) {
+    constructor(value: Int) : this(value.toBigDecimal())
+
+    override fun toString() = value.toString()
+}
